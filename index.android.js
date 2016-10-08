8c0f24c38528b89d6cd9640ba6553dff1f85379d
Samina Chhatwal <csaminak@gmail.com>
2016-10-08 10:54:22 -0400

    auto indented

--- index.android.js
+++ index.android.js
@@ -1,8 +1,8 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+* @flow
+*/
 
 import React, { Component } from 'react'
 import {
@@ -16,16 +16,16 @@ class SaveTheWild extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+      <Text style={styles.welcome}>
+      Welcome to React Native!
+      </Text>
+      <Text style={styles.instructions}>
+      To get started, edit index.android.js
+      </Text>
+      <Text style={styles.instructions}>
+      Double tap R on your keyboard to reload,{'\n'}
+      Shake or press menu button for dev menu
+      </Text>
       </View>
     )
   }
