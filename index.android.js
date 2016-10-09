cdde60420a41029bae9ee6f7db38d5cfadc42300
Samina Chhatwal <csaminak@gmail.com>
2016-10-08 20:02:33 -0400

    Comment out call to Map and as yet fictional Crime Report

--- index.android.js
+++ index.android.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import { AppRegistry, Navigator } from 'react-native'
 import CrimePicker from './components/crime-picker'
-import Map from './components/map'
+// import Map from './components/map'
 
 class SaveTheWild extends Component {
   renderScene (route, navigator) {
     switch (route.id) {
       case 'first':
         return <CrimePicker navigator={navigator} title='Choose a type of crime' />
-      case 'second':
-        return <Map navigator={navigator} title='Show us the scene of the crime' />
-      case 'third':
-        return <CrimeReport navigator={navigator} title='Finalize and submit crime report' />
+      // case 'second':
+      //   return <Map navigator={navigator} title='Show us the scene of the crime' />
+      // case 'third':
+      //   return <CrimeReport navigator={navigator} title='Finalize and submit crime report' />
     }
   }
 
