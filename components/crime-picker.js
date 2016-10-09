876fb9d1b6fbb17305f1d7a31f4c946645e0f1b5
Samina Chhatwal <csaminak@gmail.com>
2016-10-09 00:01:25 -0400

    added styling

--- components/crime-picker.js
+++ components/crime-picker.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react'
-import { Picker, View } from 'react-native'
+import { Picker, StyleSheet, View } from 'react-native'
 
 class CrimePicker extends Component {
   render () {
     return (
-      <View ref={component => this._root = component}>
+      <View style={styles.selectCrime} ref={component => this._root = component}>
         <Picker
-          selectedValue={ this.props.selectedCrime }
-          // onValueChange={ (selected) => this.props.onChange(selected) }>
-          style={{width: 300}}>
+          style={styles.selectedCrimePicker}
+          selectedValue={ this.props.selectedCrime }>
           <Picker.Item label='Poaching and Trafficking' value='poaching' />
           <Picker.Item label='Logging and Plants' value='logging' />
           <Picker.Item label='Fishing (IUUF)' value='fishing' />
@@ -17,5 +16,21 @@ class CrimePicker extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  selectCrime: {
+    width: 300,
+    paddingTop: 0,
+    paddingBottom: 0,
+    backgroundColor: '#5E676E',
+    borderColor: '#3B3B3B',
+    borderWidth: 2,
+    borderRadius: 5
+  },
+  selectedCrimePicker: {
+    color: '#E1A600'
+  }
+});
+
 
 export default CrimePicker
--- index.android.js
+++ index.android.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { AppRegistry, Navigator, View } from 'react-native'
+import { AppRegistry, Navigator, View, StyleSheet } from 'react-native'
 import Button from 'react-native-button'
 import CrimePicker from './components/crime-picker'
 import Map from './components/map'
@@ -34,10 +34,10 @@ class SaveTheWild extends Component {
     <Navigator
       initialRoute={routes[0]}
       renderScene={(route, navigator) =>
-        <View>
+        <View style={styles.container}>
           {route.component}
           <Button
-            style={{width: 200, height: 200, backgroundColor: 'black'}}
+            style={styles.continueButton}
             onPress={() => {
               const nextPage = route.index++
               navigator.push(routes[nextPage])
@@ -46,10 +46,30 @@ class SaveTheWild extends Component {
           </Button>
         </View>
       }
-      style={{padding: 100}}
     />
   )
   }
 }
+
+const styles = StyleSheet.create({
+  continueButton: {
+    width: 100,
+    color: '#E1A600',
+    backgroundColor: '#5E676E',
+    borderColor: '#3B3B3B',
+    borderWidth: 2,
+    borderRadius: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    marginTop: 20
+  },
+  container: {
+    flex:1,
+    alignItems:'center',
+    justifyContent:'center',
+    backgroundColor: '#4d93d1'
+  }
+});
+
 
 AppRegistry.registerComponent('SaveTheWild', () => SaveTheWild)
